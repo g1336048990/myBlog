097dfb52b7c1b9b4d8085c16d54bee9d19a5d8fb
g1336048990 <1336048990@qq.com>
2019-02-18 22:40:16 +0800

    毕业设计

---
+++ webpack.server.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const htmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack')
+module.exports = {
+    entry: {
+        main: './src/main.js',
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'build.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: 'style-loader!css-loader!autoprefixer-loader'
+            },{
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!autoprefixer-loader!less-loader'
+            },{
+                test: /\.(jpg|png|eot|svg|ttf|woff|woff2|gif)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 4096, //4096字节以上生成文件，否则base64
+                    name: '[name].[ext]'
+                }
+            },{
+                test: /\.js$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+            },{
+                test: /vue-preview.src.*?js$/,
+                loader: 'babel-loader',
+            },{
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new htmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new webpack.ProvidePlugin({
+            jQuery: "jquery",
+            $: "jquery"
+        })
+    ]
+}
